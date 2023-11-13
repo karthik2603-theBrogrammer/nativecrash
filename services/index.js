02a085f02c7023ee7247f93abdf706c0ba132848
Larry8668 <114809719+Larry8668@users.noreply.github.com>
2023-11-13 20:06:00 +0530

    Update index.js

--- services/index.js
+++ services/index.js
@@ -101,6 +101,247 @@ con.connect(function (err) {
     if (err) throw err;
     console.log("Crash Report table created or already exists");
   });
+
+  con.query(`
+  CREATE TABLE IF NOT EXISTS your_endpoint_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    error_title TEXT NOT NULL,
+    error_description TEXT NOT NULL,
+    brand VARCHAR(255) NOT NULL,
+    device_name VARCHAR(255) NOT NULL,
+    is_device BOOLEAN NOT NULL,
+    manufacturer VARCHAR(255) NOT NULL,
+    model_name VARCHAR(255) NOT NULL,
+    build_id VARCHAR(255) NOT NULL,
+    internal_build_id VARCHAR(255) NOT NULL,
+    cpu_architectures VARCHAR(255) NOT NULL,
+    total_memory VARCHAR(255) NOT NULL,
+    device_version VARCHAR(255) NOT NULL,
+    latitude VARCHAR(20) NOT NULL,
+    longitude VARCHAR(20) NOT NULL,
+    location_city VARCHAR(255) NOT NULL,
+    location_state VARCHAR(255) NOT NULL,
+    location_suburb VARCHAR(255) NOT NULL
+  )
+  `, function (err, result) {
+    if (err) throw err;
+    console.log("your_endpoint_data table created or already exists");
+  });
+
+  // const createProcedureQuery = `
+  // CREATE PROCEDURE IF NOT EXISTS your_procedure_name()
+  // BEGIN
+  //   -- Your procedure logic here
+  // END;
+  // `;
+  
+  // con.query(createProcedureQuery, (err, results) => {
+  //   if (err) throw err;
+  //   if (results.warningStatus === 0) {
+  //     console.log("Stored Procedure created successfully");
+  //   } else {
+  //     console.log("Stored Procedure already exists");
+  //   }
+  // });
+  
+  con.query(
+    `CREATE PROCEDURE IF NOT EXISTS InsertDeviceSurfaceInfo(
+      IN p_build_id VARCHAR(255),
+      IN p_brand VARCHAR(255),
+      IN p_device_name VARCHAR(255),
+      IN p_device_version VARCHAR(255),
+      IN p_manufacturer VARCHAR(255),
+      IN p_model_name VARCHAR(255),
+      IN p_cpu_architectures VARCHAR(255),
+      IN p_time TIMESTAMP,
+      IN p_uptime TIMESTAMP
+    )
+    BEGIN
+      INSERT INTO device_surface_info (
+        build_id, brand, device_name, device_version,
+        manufacturer, model_name, cpu_architectures, uptime
+      )
+      VALUES (
+        p_build_id, p_brand, p_device_name, p_device_version,
+        p_manufacturer, p_model_name, p_cpu_architectures, p_uptime
+      )
+      ON DUPLICATE KEY UPDATE
+        brand = p_brand,
+        device_name = p_device_name,
+        device_version = p_device_version,
+        manufacturer = p_manufacturer,
+        model_name = p_model_name,
+        cpu_architectures = p_cpu_architectures,
+        uptime = p_uptime;
+    END;`,
+    (err, results) => {
+      if (err) throw err;
+      console.log("Procedure InsertDeviceSurfaceInfo created successfully");
+    }
+  );
+
+////////////////////////////////////////
+
+con.query(
+  `CREATE PROCEDURE IF NOT EXISTS InsertLocationInfo(
+    IN p_crash_id INT,
+    IN p_build_id VARCHAR(255),
+    IN p_latitude VARCHAR(20),
+    IN p_longitude VARCHAR(20),
+    IN p_location VARCHAR(255)
+  )
+  BEGIN
+    INSERT INTO location (
+      crash_id, build_id, latitude, longitude, location
+    )
+    VALUES (
+      p_crash_id, p_build_id, p_latitude, p_longitude, p_location
+    );
+  END;`,
+  (err, results) => {
+    if (err) throw err;
+    console.log("Procedure InsertLocationInfo created successfully");
+  }
+);
+
+// Procedure to Insert into number_of_crashes
+con.query(
+  `CREATE PROCEDURE IF NOT EXISTS InsertNumberOfCrashesInfo(
+    IN p_build_id VARCHAR(255),
+    IN p_number_of_crashes INT
+  )
+  BEGIN
+    INSERT INTO number_of_crashes (
+      build_id, number_of_crashes
+    )
+    VALUES (
+      p_build_id, p_number_of_crashes
+    )
+    ON DUPLICATE KEY UPDATE
+      number_of_crashes = number_of_crashes + p_number_of_crashes;
+  END;`,
+  (err, results) => {
+    if (err) throw err;
+    console.log("Procedure InsertNumberOfCrashesInfo created successfully");
+  }
+);
+
+// Procedure to Insert into device_detailed_information
+con.query(
+  `CREATE PROCEDURE IF NOT EXISTS InsertDetailedInformation(
+    IN p_build_id VARCHAR(255),
+    IN p_real_or_fake ENUM('real', 'fake'),
+    IN p_internal_build_id VARCHAR(255)
+  )
+  BEGIN
+    INSERT INTO device_detailed_information (
+      build_id, real_or_fake, internal_build_id
+    )
+    VALUES (
+      p_build_id, p_real_or_fake, p_internal_build_id
+    )
+    ON DUPLICATE KEY UPDATE
+      real_or_fake = p_real_or_fake,
+      internal_build_id = p_internal_build_id;
+  END;`,
+  (err, results) => {
+    if (err) throw err;
+    console.log("Procedure InsertDetailedInformation created successfully");
+  }
+);
+
+// Procedure to Insert into crash_info
+con.query(
+  `CREATE PROCEDURE IF NOT EXISTS InsertCrashReport(
+    IN p_build_id VARCHAR(255),
+    IN p_crash_id INT,
+    IN p_error_title TEXT,
+    IN p_error_description TEXT
+  )
+  BEGIN
+    INSERT INTO crash_info (
+      build_id, crash_id, error_title, error_description
+    )
+    VALUES (
+      p_build_id, p_crash_id, p_error_title, p_error_description
+    );
+  END;`,
+  (err, results) => {
+    if (err) throw err;
+    console.log("Procedure InsertCrashReport created successfully");
+  }
+);
+//   con.query(`
+// DELIMITER //
+// CREATE TRIGGER InsertYourEndpointData
+// AFTER INSERT ON your_endpoint_data
+// FOR EACH ROW
+// BEGIN
+//   CALL InsertDeviceSurfaceInfo(
+//     NEW.build_id, NEW.brand, NEW.device_name, NEW.device_version,
+//     NEW.manufacturer, NEW.model_name, NEW.build_id, NEW.internal_build_id,
+//     NEW.cpu_architectures, NEW.total_memory, NEW.time, NEW.time
+//   );
+
+//   CALL InsertLocationInfo(
+//     NEW.id, NEW.build_id, NEW.latitude, NEW.longitude,
+//     CONCAT(NEW.location_city, ', ', NEW.location_state, ', ', NEW.location_suburb)
+//   );
+
+//   CALL InsertNumberOfCrashesInfo(
+//     NEW.build_id, 1 -- Assuming 1 as the default number_of_crashes for each entry
+//   );
+
+//   CALL InsertDetailedInformation(
+//     NEW.build_id, 'real', NEW.internal_build_id
+//   );
+
+//   CALL InsertCrashReport(
+//     NEW.build_id, NEW.id, NEW.error_title, NEW.error_description
+//   );
+// END //
+// DELIMITER ;
+//   `, function (err, result) {
+//     if (err) throw err;
+//     console.log("InsertYourEndpointData table created or already exists");
+//   });
+
+const triggerQuery = `
+    CREATE TRIGGER IF NOT EXISTS InsertYourEndpointData
+    AFTER INSERT ON your_endpoint_data
+    FOR EACH ROW
+    BEGIN
+      CALL InsertDeviceSurfaceInfo(
+        NEW.build_id, NEW.brand, NEW.device_name, NEW.device_version,
+        NEW.manufacturer, NEW.model_name, NEW.cpu_architectures, NEW.time, NEW.time
+      );
+
+      CALL InsertLocationInfo(
+        NEW.id, NEW.build_id, NEW.latitude, NEW.longitude,
+        CONCAT(NEW.location_city, ', ', NEW.location_state, ', ', NEW.location_suburb)
+      );
+
+      CALL InsertNumberOfCrashesInfo(
+        NEW.build_id, 1 -- Assuming 1 as the default number_of_crashes for each entry
+      );
+
+      CALL InsertDetailedInformation(
+        NEW.build_id, 'real', NEW.internal_build_id
+      );
+
+      CALL InsertCrashReport(
+        NEW.build_id, NEW.id, NEW.error_title, NEW.error_description
+      );
+    END;
+  `;
+
+  con.query(triggerQuery, (err, results) => {
+    if (err) throw err;
+    console.log("Trigger InsertYourEndpointData created successfully");
+  });
+
+
 });
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -125,9 +366,27 @@ app.post('/v2/log-crash', async (req, res) => {
       locationData: { city, state, suburb },
       coordinates: { latitude, longitude }
     } = req.body;
-  
+    
     console.log(data)
     res.status(200).send({})
+    con.query(
+      `CALL InsertYourEndpointData(
+        '${buildId}', '${brand}', '${deviceName}', '${deviceVersion}',
+        '${manufacturer}', '${modelName}', '${cpuArchitectures}', '${time}', '${time}',
+        '${city}', '${state}', '${suburb}', '${latitude}', '${longitude}', 1,
+        '${internalBuildId}', 'real', '${internalBuildId}',
+        '${buildId}', 1, '${errorTitle}', '${errorDescription}'
+      );`,
+      (err, results) => {
+        if (err) {
+          console.error("Error executing stored procedure:", err);
+          res.status(500).send({});
+        } else {
+          console.log("Stored procedure executed successfully");
+          res.status(200).send({});
+        }
+      }
+    );
   } catch (error) {
     res.status(500).send({})
   }
